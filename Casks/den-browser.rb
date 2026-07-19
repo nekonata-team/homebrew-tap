e6c04b19be94763f372675fcc71fee7533e2208a
tyPhoon <typhoonmoon1022@gmail.com>
2026-07-19 22:50:02 +0900

    den-browser 0.1.0 (new cask)

---
+++ Casks/den-browser.rb
@@ -0,0 +1,23 @@
+cask "den-browser" do
+  version "0.1.0"
+  sha256 "cd142abd9f692c578f7120c38d6cfcc704979a2783f055fb2202f368399022b8"
+
+  url "https://github.com/nekonata-team/den-browser/releases/download/v#{version}/Den-Browser-#{version}-macOS.zip",
+      verified: "github.com/nekonata-team/den-browser/"
+  name "Den Browser"
+  desc "Niri-inspired spatial browser for persistent web work"
+  homepage "https://nekonata.dev/"
+
+  depends_on macos: :tahoe
+
+  app "Den Browser.app"
+
+  zap trash: [
+    "~/Library/Application Support/Den Browser",
+    "~/Library/Caches/dev.nekonata.denbrowser",
+    "~/Library/Containers/dev.nekonata.denbrowser",
+    "~/Library/HTTPStorages/dev.nekonata.denbrowser.binarycookies",
+    "~/Library/Preferences/dev.nekonata.denbrowser.plist",
+    "~/Library/WebKit/dev.nekonata.denbrowser",
+  ]
+end
