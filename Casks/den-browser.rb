0be4f06dd8bafa86feba1d6123e1b36d027282e6
tyPhoon <typhoonmoon1022@gmail.com>
2026-07-24 08:14:20 +0900

    den-browser 0.5.1

--- Casks/den-browser.rb
+++ Casks/den-browser.rb
@@ -1,6 +1,6 @@
 cask "den-browser" do
-  version "0.5.0"
-  sha256 "d599ee7a098ba4f2e51e22bf956fc12b79b7b744307e2769b888f7deb6f4ba60"
+  version "0.5.1"
+  sha256 "04fb45b3d1241bb435d756d6983cf34bf2a0a8f568d1db79f923873ec14e110f"
 
   url "https://github.com/nekonata-team/den-browser/releases/download/v#{version}/Den-Browser-#{version}-macOS.zip",
       verified: "github.com/nekonata-team/den-browser/"
